b4af9fac67c6e7bb9de30cfdb6002097bfa57a2e
Eduardoss45 <temporariac432@gmail.com>
2024-06-29 15:01:41 -0300

    Alterações no layout e novos tipos de dados criados para a API

---
+++ front-end/src/components/Acomodacoes.jsx
@@ -0,0 +1,139 @@
+import Anuncio from "../image/anuncio.jpg"
+
+import "./Acomodacoes.css";
+
+const Main = () => {
+  return (
+    <>
+      <div id="filtros">
+        <div>
+          <button className="opt-filtros">Pousada</button>
+        </div>
+        <div>
+          <button className="opt-filtros">Chalé</button>
+        </div>
+        <div>
+          <button className="opt-filtros">Apto</button>
+        </div>
+        <div>
+          <button className="opt-filtros">Casa</button>
+        </div>
+        <div>
+          <button className="opt-filtros">Quarto</button>
+        </div>
+      </div>
+      <div id="area-anuncio">
+        <div className="anuncio">
+            <div className="img-anuncio">
+                <img src={Anuncio} alt="Imagem da acomodação" />
+                <span>Status</span>
+            </div>
+            <p><strong>Nome do lugar</strong></p>
+            <p>Descrição do lugar</p>
+            <p><strong>R$0</strong> Preço por hospede</p>
+        </div>
+        <div className="anuncio">
+            <div className="img-anuncio">
+                <img src={Anuncio} alt="Imagem da acomodação" />
+                <span>Status</span>
+            </div>
+            <p><strong>Nome do lugar</strong></p>
+            <p>Descrição do lugar</p>
+            <p><strong>R$0</strong> Preço por hospede</p>
+        </div>
+        <div className="anuncio">
+            <div className="img-anuncio">
+                <img src={Anuncio} alt="Imagem da acomodação" />
+                <span>Status</span>
+            </div>
+            <p><strong>Nome do lugar</strong></p>
+            <p>Descrição do lugar</p>
+            <p><strong>R$0</strong> Preço por hospede</p>
+        </div>
+        <div className="anuncio">
+            <div className="img-anuncio">
+                <img src={Anuncio} alt="Imagem da acomodação" />
+                <span>Status</span>
+            </div>
+            <p><strong>Nome do lugar</strong></p>
+            <p>Descrição do lugar</p>
+            <p><strong>R$0</strong> Preço por hospede</p>
+        </div>
+        <div className="anuncio">
+            <div className="img-anuncio">
+                <img src={Anuncio} alt="Imagem da acomodação" />
+                <span>Status</span>
+            </div>
+            <p><strong>Nome do lugar</strong></p>
+            <p>Descrição do lugar</p>
+            <p><strong>R$0</strong> Preço por hospede</p>
+        </div>
+        <div className="anuncio">
+            <div className="img-anuncio">
+                <img src={Anuncio} alt="Imagem da acomodação" />
+                <span>Status</span>
+            </div>
+            <p><strong>Nome do lugar</strong></p>
+            <p>Descrição do lugar</p>
+            <p><strong>R$0</strong> Preço por hospede</p>
+        </div>
+        <div className="anuncio">
+            <div className="img-anuncio">
+                <img src={Anuncio} alt="Imagem da acomodação" />
+                <span>Status</span>
+            </div>
+            <p><strong>Nome do lugar</strong></p>
+            <p>Descrição do lugar</p>
+            <p><strong>R$0</strong> Preço por hospede</p>
+        </div>
+        <div className="anuncio">
+            <div className="img-anuncio">
+                <img src={Anuncio} alt="Imagem da acomodação" />
+                <span>Status</span>
+            </div>
+            <p><strong>Nome do lugar</strong></p>
+            <p>Descrição do lugar</p>
+            <p><strong>R$0</strong> Preço por hospede</p>
+        </div>
+        <div className="anuncio">
+            <div className="img-anuncio">
+                <img src={Anuncio} alt="Imagem da acomodação" />
+                <span>Status</span>
+            </div>
+            <p><strong>Nome do lugar</strong></p>
+            <p>Descrição do lugar</p>
+            <p><strong>R$0</strong> Preço por hospede</p>
+        </div>
+        <div className="anuncio">
+            <div className="img-anuncio">
+                <img src={Anuncio} alt="Imagem da acomodação" />
+                <span>Status</span>
+            </div>
+            <p><strong>Nome do lugar</strong></p>
+            <p>Descrição do lugar</p>
+            <p><strong>R$0</strong> Preço por hospede</p>
+        </div>
+        <div className="anuncio">
+            <div className="img-anuncio">
+                <img src={Anuncio} alt="Imagem da acomodação" />
+                <span>Status</span>
+            </div>
+            <p><strong>Nome do lugar</strong></p>
+            <p>Descrição do lugar</p>
+            <p><strong>R$0</strong> Preço por hospede</p>
+        </div>
+        <div className="anuncio">
+            <div className="img-anuncio">
+                <img src={Anuncio} alt="Imagem da acomodação" />
+                <span>Status</span>
+            </div>
+            <p><strong>Nome do lugar</strong></p>
+            <p>Descrição do lugar</p>
+            <p><strong>R$0</strong> Preço por hospede</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;

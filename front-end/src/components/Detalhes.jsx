6895e437ce6a0e178de2c3fb860e8d706a565469
Eduardoss45 <temporariac432@gmail.com>
2024-06-30 19:10:51 -0300

    Criação de dados no back-end, consumo no front-end

---
+++ front-end/src/components/Detalhes.jsx
@@ -0,0 +1,24 @@
+import "./Detalhes.css";
+
+const Detalhes = ({ image, status, name, description, price }) => {
+  return (
+    <div className="anuncio">
+      <div className="img-anuncio">
+        <img
+          src={`http://127.0.0.1:8000/${image}`}
+          alt="Imagem da acomodação"
+        />
+        <span>Status: {status ? "Disponível" : "Indisponível"}</span>
+      </div>
+      <p>
+        <strong>{name}</strong>
+      </p>
+      <p>{description}</p>
+      <p>
+        <strong>R${price}</strong> Preço por hospede
+      </p>
+    </div>
+  );
+};
+
+export default Detalhes;
